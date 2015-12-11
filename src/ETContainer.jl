f413a36e527bfb83b8b1779f7e803dd74196aa22
Tyler Olsen <tjolsen@mit.edu>
2015-12-11 17:56:58 -0500

    added manual (8x) loop unrolling to the ETContainer constructor. This makes it unambiguously beat a non-unrolled hand-looped equivalent expression.

--- src/ETContainer.jl
+++ src/ETContainer.jl
@@ -6,8 +6,19 @@ end
 
 function ETContainer{T,R}(A::VectorizedExpression{T,R})
     dat = Array{T,R}(size(A)...)
-    for i = 1:length(dat)
+    L = length(dat)
+    for i = 1:8:8*Int64(floor(L/8))
         @inbounds dat[i] = A[i]
+        @inbounds dat[i+1] = A[i+1]
+        @inbounds dat[i+2] = A[i+2]
+        @inbounds dat[i+3] = A[i+3]
+        @inbounds dat[i+3] = A[i+4]
+        @inbounds dat[i+3] = A[i+5]
+        @inbounds dat[i+3] = A[i+6]
+        @inbounds dat[i+3] = A[i+7]
+    end
+    for i = 8*Int64(floor(L/8)):L
+        @inbounds dat[i] = A[i]        
     end
     return ETContainer(dat)
 end
